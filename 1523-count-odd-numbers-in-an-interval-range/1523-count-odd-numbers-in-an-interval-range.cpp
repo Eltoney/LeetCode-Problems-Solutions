d258b774da1d7c6af9c4b680126f0973492ac62a
Ali Ahmed Mohamed  Eltoney <aliahmedeltoney@gmail.com>
2022-06-14 05:50:51 +0200

    Time: 3 ms (46.26%), Space: 5.9 MB (25.03%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int ans = (high - low) / 2;
-        ans += (low % 2) || (high % 2);
        
-        return ans;
+        return  (high - low) / 2 + ((low % 2) || (high % 2)) ;
     }
 };
