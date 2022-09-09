13fe539461112ebd19c1ac6f090d90d3d5e31b70
Ali Ahmed Mohamed  Eltoney <aliahmedeltoney@gmail.com>
2022-09-09 14:40:34 +0200

    Time: 1527 ms (11.85%), Space: 125 MB (98.94%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,22 +1,19 @@
 class Solution {
-    
-
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
         sort(properties.begin() , properties.end());
-        set<int> st;
         int ans = 0;
         int n = properties.size();
         int cur = n - 1;
         int j = cur;
+        int mx = INT_MIN;
         while(cur >= 0){
             while(j >= 0 && properties[j][0] == properties[cur][0]){
-                auto it = st.upper_bound(properties[j][1]);
-                ans += (it != st.end());
+                ans += properties[j][1] < mx; 
                 j--;
             }
             while(cur > j){
-                st.insert(properties[cur][1]);
+                mx = max(mx , properties[cur][1]);
                 cur--;
             }
         }
