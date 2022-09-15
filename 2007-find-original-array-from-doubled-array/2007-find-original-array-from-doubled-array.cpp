29aec079e9a11b1103e1b8179d055f49b7202ee3
Ali Ahmed Mohamed  Eltoney <aliahmedeltoney@gmail.com>
2022-09-15 09:20:56 +0200

    Time: 1111 ms (9.69%), Space: 144.2 MB (45.11%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        vector<int> cnt(200009);
-        sort(changed.begin() , changed.end());
-        vector<int> ans;
-        bool isOk = 1;
+        map<int,int> cnt;
         int n = changed.size();
         for(int i = 0 ; i < n ; i++){
             cnt[changed[i]]++;
         }
-        
-        for(int i = 0 ; i < n ; i++){
-            if(cnt[changed[i]]){
+        vector<int> ans;
+        sort(changed.begin() , changed.end());
+        for(int i = 0 ; i  < n ; i++){
+            if(cnt[changed[i]] > 0){
                 int need = changed[i] * 2;
-                if(need == changed[i]){
-                    if(cnt[need] > 1){
-                        cnt[need] -= 2;
-                        ans.push_back(need);
-                    }else{
-                        isOk = 0;
-                        break;
-                    }
+                if(!need && cnt[need] > 1){
+                    ans.push_back(need);
+                    cnt[need] -= 2;
+                }else if(need && cnt[need] > 0){
+                    ans.push_back(changed[i]);
+                    cnt[need]--;
+                    cnt[changed[i]]--;
                 }else{
-                    if(cnt[need] > 0){
-                        ans.push_back(changed[i]);
-                        cnt[need]--;
-                        cnt[changed[i]]--;
-                    }else{
-                        isOk = 0;
-                        break;
-                    }
+                    ans = vector<int>();
+                    break;
                 }
-
             }
-        }
-        
-        if(!isOk){
-            ans = vector<int>();
         }
         return ans;
     }
